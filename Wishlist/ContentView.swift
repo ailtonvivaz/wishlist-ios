6e919575a8f9b0d89c70c97d066ca1cee59161d2
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2020-02-09 19:15:15 -0300

    Initial Commit

---
+++ Wishlist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wishlist
+//
+//  Created by Ailton Vieira Pinto Filho on 09/02/20.
+//  Copyright © 2020 Veevaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
